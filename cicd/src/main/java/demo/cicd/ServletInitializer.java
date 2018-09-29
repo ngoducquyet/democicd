11d1d55fe2a5ff3e932867f95360830cc135021c
quyetngo <quyetngo@gmail.com>
2018-09-29 23:02:26 +0700

    no message

---
+++ cicd/src/main/java/demo/cicd/ServletInitializer.java
@@ -0,0 +1,13 @@
+package demo.cicd;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CicdApplication.class);
+	}
+
+}
